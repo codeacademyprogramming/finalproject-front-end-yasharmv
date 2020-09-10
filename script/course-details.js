d8cddcbabdc8c8393a9dc99afcf91408a2d94903
yasharmv <49427372+yasharmv@users.noreply.github.com>
2020-09-11 00:31:33 +0400

    course details

---
+++ script/course-details.js
@@ -0,0 +1,33 @@
+$(".owl1").owlCarousel({
+  loop: true,
+  margin: 50,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 5,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".owl7").owlCarousel({
+    loop: true,
+    margin: 50,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 5,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
